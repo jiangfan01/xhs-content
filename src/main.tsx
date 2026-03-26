7681b43d4258a7142090170c221466e70a6de397
mzoob <mzoob_ux@163.com>
2026-03-26 10:41:05 +0800

    fix: align build config after rebase

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
 import { AuthProvider } from "~/contexts/AuthContext";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
 
 const queryClient = new QueryClient({
   defaultOptions: {
